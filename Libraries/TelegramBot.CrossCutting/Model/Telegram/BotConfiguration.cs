b896279c9fdbf71dfa3fd610d6385a6101d870f3
Fabio Braga <fabiobraga.net@gmail.com>
2020-02-15 11:52:20 -0300

    Start do projeto

---
+++ Libraries/TelegramBot.CrossCutting/Model/Telegram/BotConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBot.CrossCutting.Model.Telegram
+{
+    public class BotConfiguration
+    {
+        public string BotToken { get; set; } = "823321556:AAFH3TZsLJAYyas4XiyjyEkoqdGFZvTXxU4";
+
+        public string Socks5Host { get; set; } = string.Empty;
+
+        public int Socks5Port { get; set; }
+    }
+}
